9549b3ddc5357e97b676128eb13dc4dd41a22b88
unknown <shubhamehetre>
2025-05-28 13:37:15 +0530

    second commit

---
+++ src/main/java/spring/boot/docker/spring/boot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package spring.boot.docker.spring.boot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+	@GetMapping("/message")
+	public String getResult(){
+		return "Working fine";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/docker/spring/boot/docker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.docker.spring.boot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
