f8b187a18db9c6dd23b82a5bfa51ae0f857c4c02
unknown <shubhamehetre>
2025-05-28 14:06:55 +0530

    third build

--- src/main/java/spring/boot/docker/spring/boot/docker/SpringBootDockerApplication.java
+++ src/main/java/spring/boot/docker/spring/boot/docker/SpringBootDockerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringBootDockerApplication {
 	@GetMapping("/message")
 	public String getResult(){
+		System.out.println("Hello");
 		return "Working fine";
 	}
 
